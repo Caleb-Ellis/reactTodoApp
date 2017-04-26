872c84402852a7654a36966ccec150c6727c73ed
CEllis <tehwavy@gmail.com>
2017-04-26 10:44:56 +1000

    Modularised css/js - added add and delete functionality

--- src/app/index.js
+++ src/app/index.js
@@ -1,30 +1,57 @@
-
 var React = require('react');
 var ReactDOM = require('react-dom');
+require('./css/index.css');
+
+// Module requires
+var TodoItem = require('./todoItem');
+var AddItem = require('./addItem');
 
 // Create component
 var TodoComponent = React.createClass({
 
   getInitialState: function() {
     return {
-      todos: ['wash up', 'eat some cheese', 'take a nap']
+      todos: ['wash up', 'eat some cheese', 'take a nap', 'buy flowers', 'clean'],
     }
   },
 
   render: function() {
+
+    var todos = this.state.todos;
+
+    todos = todos.map(function(item, index) {
+      return(
+        <TodoItem item={item} key={index} onDelete={this.onDelete}/>
+      );
+    }.bind(this));
+
     return(
-      <div>
+      <div id="todo-list">
         <p>Crap I need to do...</p>
-        <ul>
-          <li>{this.state.todos[0]}</li>
-          <li>{this.state.todos[1]}</li>
-          <li>{this.state.todos[2]}</li>
-        </ul>
+        <ul>{todos}</ul>
+        <AddItem onAdd={this.onAdd}/>
       </div>
     );
-  } // render
-});
+  }, // render
 
+  // Custom functions
+  onDelete: function(item) {
+    var updatedTodos = this.state.todos.filter(function(val, index) {
+      return item !== val;
+    });
+    this.setState({
+      todos: updatedTodos
+    });
+  },
+
+  onAdd: function(item) {
+    var updatedTodos = this.state.todos;
+    updatedTodos.push(item);
+    this.setState({
+      todos: updatedTodos
+    });
+  }
+});
 
 // Put component into html page
 ReactDOM.render(<TodoComponent mssg="Hey guys!"/>, document.getElementById('todo-wrapper'));
